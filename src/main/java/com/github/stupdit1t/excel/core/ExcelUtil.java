9faba2f200c741d9b1473b1857e8f61b24306498
625 <353613274@qq.com>
2022-08-09 22:59:32 +0800

    fixbug 1.重复表头处理

--- src/main/java/com/github/stupdit1t/excel/core/ExcelUtil.java
+++ src/main/java/com/github/stupdit1t/excel/core/ExcelUtil.java
@@ -797,7 +797,7 @@ public class ExcelUtil {
 
             // 处理重复表头
             if(text.startsWith("$")){
-                text = text.replaceAll("\\$_\\d+_","");
+                text = text.replaceAll("^\\$_\\d+_","");
             }
             CellUtil.createCell(sheet.getRow(headerIndex), i, text, styleNew);
         }
