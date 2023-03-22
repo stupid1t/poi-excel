939476c5af2f73de665c3c7ec2d12e5481d40e0c
stupid1t <damon.li@yamibuy.com>
2023-03-22 10:12:16 +0800

    版本同步

--- src/main/java/com/github/stupdit1t/excel/core/parse/XSSFSheetXMLHandler.java
+++ src/main/java/com/github/stupdit1t/excel/core/parse/XSSFSheetXMLHandler.java
@@ -1,8 +1,6 @@
 package com.github.stupdit1t.excel.core.parse;
 
-import org.apache.poi.ss.usermodel.DataFormatter;
 import org.apache.poi.xssf.eventusermodel.ReadOnlySharedStringsTable;
-import org.apache.poi.xssf.model.CommentsTable;
 import org.apache.poi.xssf.model.StylesTable;
 import org.xml.sax.SAXException;
 
@@ -12,16 +10,6 @@ import org.xml.sax.SAXException;
 public class XSSFSheetXMLHandler extends org.apache.poi.xssf.eventusermodel.XSSFSheetXMLHandler {
 
     private final org.apache.poi.xssf.eventusermodel.XSSFSheetXMLHandler.SheetContentsHandler output;
-
-    public XSSFSheetXMLHandler(StylesTable styles, CommentsTable comments, ReadOnlySharedStringsTable strings, SheetContentsHandler sheetContentsHandler, DataFormatter dataFormatter, boolean formulasNotResults) {
-        super(styles, comments, strings, sheetContentsHandler, dataFormatter, formulasNotResults);
-        this.output = sheetContentsHandler;
-    }
-
-    public XSSFSheetXMLHandler(StylesTable styles, ReadOnlySharedStringsTable strings, SheetContentsHandler sheetContentsHandler, DataFormatter dataFormatter, boolean formulasNotResults) {
-        super(styles, strings, sheetContentsHandler, dataFormatter, formulasNotResults);
-        this.output = sheetContentsHandler;
-    }
 
     public XSSFSheetXMLHandler(StylesTable styles, ReadOnlySharedStringsTable strings, SheetContentsHandler sheetContentsHandler, boolean formulasNotResults) {
         super(styles, strings, sheetContentsHandler, formulasNotResults);
